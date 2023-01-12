fcd36951ab2bf0a4b2702bfe803d715845dae34c
Kurogoma939 <kan.sss.939@gmail.com>
2023-01-12 23:27:53 +0900

    フォント追加

--- lib/utils/router/router.dart
+++ lib/utils/router/router.dart
@@ -31,35 +31,36 @@ final router = GoRouter(
 
     /// アプリホーム
     GoRoute(
-        name: SampleHomePage.routeName,
-        path: SampleHomePage.routePath,
-        pageBuilder: (context, state) => NoTransitionPage(
-              key: state.pageKey,
-              child: const SampleHomePage(),
-            ),
-        routes: [
-          // ネストかつアニメーション込みの画面遷移
-          GoRoute(
-            name: SampleDetailPage.routeName,
-            path: SampleDetailPage.routePath,
-            pageBuilder: (context, state) => CustomTransitionPage(
-              key: state.pageKey,
-              child: const SampleDetailPage(),
-              transitionDuration: const Duration(milliseconds: 300),
-              transitionsBuilder:
-                  (context, animation, secondaryAnimation, child) {
-                return SlideTransitionBuilder().buildTransitions(
-                  MaterialPageRoute(
-                    builder: (context) => const SampleDetailPage(),
-                  ),
-                  context,
-                  animation,
-                  secondaryAnimation,
-                  child,
-                );
-              },
-            ),
+      name: SampleHomePage.routeName,
+      path: SampleHomePage.routePath,
+      pageBuilder: (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        child: const SampleHomePage(),
+      ),
+      routes: [
+        // ネストかつアニメーション込みの画面遷移
+        GoRoute(
+          name: SampleDetailPage.routeName,
+          path: SampleDetailPage.routePath,
+          pageBuilder: (context, state) => CustomTransitionPage(
+            key: state.pageKey,
+            child: const SampleDetailPage(),
+            transitionDuration: const Duration(milliseconds: 300),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return SlideTransitionBuilder().buildTransitions(
+                MaterialPageRoute(
+                  builder: (context) => const SampleDetailPage(),
+                ),
+                context,
+                animation,
+                secondaryAnimation,
+                child,
+              );
+            },
           ),
-        ]),
+        ),
+      ],
+    ),
   ],
 );
